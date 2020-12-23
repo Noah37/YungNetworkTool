6cb975c8a2144645d78c60622113e0e069cd956a
Noah <caonongyun@guazi.com>
2020-12-23 16:31:02 +0800

    fix bug

--- YungNetworkTool/Classes/DataTask.swift
+++ YungNetworkTool/Classes/DataTask.swift
@@ -10,7 +10,9 @@ import Alamofire
 
 open class DataTask: Task<DataTask> {
     
-    public var resultHandler:TaskResultHandler?
+    public var resultHandler:TaskResultHandler? {
+        didSet { dataResult(closure: self.resultHandler) }
+    }
     
     public var responseData:Data? { return response?.value }
     
@@ -18,10 +20,14 @@ open class DataTask: Task<DataTask> {
     
     override public func resume() {
         super.resume()
+    }
+    
+    private func dataResult(closure:TaskResultHandler?) {
+        guard let resultHandler = closure else { return }
         guard let req = request as? DataRequest else { return }
-        req.responseData { [weak self] (data) in
-            self?.response = data
-            self?.resultHandler?(data.value, data.error)
+        req.responseData { [weak self] (responseData) in
+            self?.response = responseData
+            resultHandler(responseData.value, responseData.error)
         }
     }
 }
